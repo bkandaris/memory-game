ee4687d438125147ea4a175d19ac3fea4ad498c7
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-10-22 16:08:58 -0400

    fixed filepath

--- app.js
+++ app.js
@@ -85,8 +85,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const optionTwoId = cardsChosenId[1];
 
     if (optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', '../images/Blank.png');
-      cards[optionTwoId].setAttribute('src', '../images/Blank.png');
+      cards[optionOneId].setAttribute('src', './images/Blank.png');
+      cards[optionTwoId].setAttribute('src', './images/Blank.png');
       // alerting the user that they have made an error
       swal({
         title: 'Ut oh',
@@ -102,15 +102,15 @@ document.addEventListener('DOMContentLoaded', () => {
       });
       // if a match is found, we change the image on the board to show that the user has already selected these cards then remove the event listener so that the cards cannot be clicked anymore
       // Then push the the cardChosen to the cardsWon array to keep track if the game has been finished
-      cards[optionOneId].setAttribute('src', '../images/Back.png');
-      cards[optionTwoId].setAttribute('src', '../images/Back.png');
+      cards[optionOneId].setAttribute('src', './images/Back.png');
+      cards[optionTwoId].setAttribute('src', './images/Back.png');
       cards[optionOneId].removeEventListener('click', flipCard);
       cards[optionTwoId].removeEventListener('click', flipCard);
       cardsWon.push(cardsChosen);
       // if cards do not match, we reset them to 'blank' and alert the user that they did not select two matching cards
     } else {
-      cards[optionOneId].setAttribute('src', '../images/Blank.png');
-      cards[optionTwoId].setAttribute('src', '../images/Blank.png');
+      cards[optionOneId].setAttribute('src', './images/Blank.png');
+      cards[optionTwoId].setAttribute('src', './images/Blank.png');
       swal({
         title: 'Oops!',
         text: 'Try again!',
